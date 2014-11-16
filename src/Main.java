183f715e836b73ecc0a463a92f083aa7a642b745
i-tiptyarev <t.vanek13@mail.ru>
2014-11-16 22:45:27 +0300

    4th Commit

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,44 @@ import java.util.*;
 
 public class Main {
      public static void main(String[] args) {
-         System.out.println("Hello World!");
-         Engine engine = new Engine();
-         AddSong(engine);
-         AddSong(engine);
-         System.out.println(engine.count);
-         engine.GetPlayList();
+         MainMenu();
      }
 
-    public static void AddSong(Engine engine)
+    public static void MainMenu()
     {
-        System.out.println("Enter Song Name");
+        int switcher = 0;
         Scanner in = new Scanner(System.in);
-        engine.Song = in.nextLine();
-        engine.Add();
-        System.out.println("Adding Successful");
-        engine.count++;
+        Engine engine = new Engine();
+        do {
+            System.out.println("Music Player v0.6\n Меню:\n1. Добавить композицию в список\n2. Начать воспроизведение\n3. Остановить воспроизведение\n4. Воспроизвести предыдущую композицию\n5. Воспроизвести следующую композицию \n6. Удалить композицию из списка \n7. Показать весь список \n8. Выход из программы \n");
+            switcher = in.nextInt();
+            switch (switcher) {
+                case 1:
+                    engine.Add();
+                    break;
+                case 2:
+                    engine.Play();
+                    break;
+                case 3:
+                    engine.Stop();
+                    break;
+                case 4:
+                    engine.Back();
+                    break;
+                case 5:
+                    engine.Forward();
+                    break;
+                case 6:
+                    engine.Remove();
+                    break;
+                case 7:
+                    engine.GetPlayList();
+                    break;
+                case 8:
+                    System.out.println("GoodBye!");
+                    break;
+            }
+        }while(switcher != 8);
     }
 }
 
